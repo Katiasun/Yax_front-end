9ce472930de89dba4b14ad209c92583e91d8c431
Catherine <katjatolok3@gmai.com>
2021-10-05 21:36:30 +0200

    done js

--- js/validation.js
+++ js/validation.js
@@ -9,3 +9,23 @@
     Powodzenia :)
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+function validate_form() {
+  let x = document.forms["form"]["name"].value;
+  let y = document.forms["form"]["email"].value;
+
+  if (x.length == 0) {
+    document.getElementsByTagName("input").innerHTML = "*Wypełnić pole";
+    return false;
+  }
+
+  if (y.length == 0) {
+    document.getElementsByTagName("input").innerHTML = "*Wypełnić pole";
+    return false;
+  }
+
+  if (at < 1 || dot < 1) {
+    document.getElementsByTagName("input").innerHTML =
+      "*email wpisany niepoprawnie";
+    return false;
+  }
+}
